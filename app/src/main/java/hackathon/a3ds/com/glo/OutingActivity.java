59ea25de1042bdf7d4c766bf87605e90f8b4bf40
Sofia <Zaid_Sofia@amer.dell.com>
2016-07-09 21:33:45 -0500

    Outting Activity

--- app/src/main/java/hackathon/a3ds/com/glo/OutingActivity.java
+++ app/src/main/java/hackathon/a3ds/com/glo/OutingActivity.java
@@ -1,7 +1,11 @@
 package hackathon.a3ds.com.glo;
 
 import android.app.Activity;
+import android.content.Context;
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 /**
  * Created by frank on 7/9/16.
@@ -12,5 +16,14 @@ public class OutingActivity extends Activity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_outing);
+        final Context mContext = this;
+        Button btnCancelOuting = (Button) findViewById(R.id.cancel);
+        btnCancelOuting.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent intent = new Intent(mContext, HomeActivity.class);
+                startActivity(intent);
+            }
+        });
     }
 }
