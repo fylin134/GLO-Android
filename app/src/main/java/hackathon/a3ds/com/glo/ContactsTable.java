ff4b364e3ccf21e02abe90ef4f122f3fc41e11d2
mmcurtis55 <mmcurtis55@gmail.com>
2016-07-09 20:05:41 -0500

    ContactTable

---
+++ app/src/main/java/hackathon/a3ds/com/glo/ContactsTable.java
@@ -0,0 +1,14 @@
+package hackathon.a3ds.com.glo;
+
+import android.app.Activity;
+
+import java.util.AbstractCollection;
+
+/**
+ * Created by matcurtis on 7/9/16.
+ */
+public class ContactsTable extends Activity {
+
+
+
+}
