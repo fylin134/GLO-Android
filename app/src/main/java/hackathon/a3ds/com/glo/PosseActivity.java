39a57a4e32e734d592e241a88b28ad1da15e6d7f
Sofia <Zaid_Sofia@amer.dell.com>
2016-07-09 19:50:10 -0500

    saving posse group name

--- app/src/main/java/hackathon/a3ds/com/glo/PosseActivity.java
+++ app/src/main/java/hackathon/a3ds/com/glo/PosseActivity.java
@@ -4,8 +4,12 @@ import android.app.Activity;
 import android.content.Context;
 import android.content.Intent;
 import android.os.Bundle;
+import android.text.InputType;
 import android.view.View;
 import android.widget.Button;
+import android.widget.EditText;
+
+import static hackathon.a3ds.com.glo.R.id.posse_name;
 
 /**
  * Created by frank on 7/9/16.
@@ -22,8 +26,13 @@ public class PosseActivity extends Activity{
         btnTestPosse.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
+                EditText edText1 = (EditText) findViewById(R.id.posse_name);
+                edText1.setInputType(InputType.TYPE_CLASS_TEXT);
+                String str = edText1.getText().toString();
                 Intent intent = new Intent(mContext, contactView.class);
+                intent.putExtra("posse_name",str);
                 startActivity(intent);
+
             }
         });
     }
